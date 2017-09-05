554ce42495e521c2441515ec4caab8a84ff58053
stenjus <stenjus@yandex.ru>
2017-09-06 02:49:58 +0400

    Combiner

---
+++ Assets/MeshCombinerEditor.cs
@@ -0,0 +1,22 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using Assets.Mega.Scripts;
+using UnityEditor;
+
+
+[CustomEditor(typeof(MeshCombiner))]
+public class MeshCombinerEditor : Editor {
+
+    public override void OnInspectorGUI () {
+        DrawDefaultInspector();
+        if(GUILayout.Button("Combine")) {
+            MeshCombiner ms = target as MeshCombiner;
+                ms.CombineSubMeshes();
+        }
+        if(GUILayout.Button("Clear")) {
+            MeshCombiner ms = target as MeshCombiner;
+            ms.Clear();
+        }
+    }
+}
+#endif

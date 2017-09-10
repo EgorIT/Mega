6e2e74b2c0408f5ef64ca60d1c27cd34c8178021
EgorIT <31497029+EgorIT@users.noreply.github.com>
2017-09-10 12:04:01 +0300

    *all caps

---
+++ Assets/Mega/Scripts/Interface/AllCaps.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AllCaps : MonoBehaviour {
+
+	public List<ShopCap> allCaps = new List<ShopCap>();
+	// Use this for initialization
+	void Start ()
+	{
+		allCaps = GameObject.FindObjectsOfType<ShopCap>().ToList();
+	}
+
+	public void Refresh()
+	{
+		allCaps.ForEach(x=>x.gameObject.SetActive(false));
+	}
+
+	public void Activate(string name)
+	{
+		allCaps.Where(x =>
+		{
+			if (x.name == name)
+				x.gameObject.SetActive(true);
+			return true;
+		});
+	}
+}

135b5e8bf400934a77422ade9b1c0477d53ed3de
stenjus <stenjus@yandex.ru>
2017-09-11 16:43:44 +0400

    Shops Fixes
    Lot of art fixes

---
+++ Assets/ScreenShot.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+import System;
+import System.Globalization;
+
+private var i=0;
+
+#if UNITY_EDITOR
+
+	@MenuItem ("Test tools/ScreenShot")
+	static function Screen () {
+		Application.CaptureScreenshot("KingOfDirt_ScreenShot.png",1);
+	}
+
+#endif
+
+function Start()
+{
+	Debug.Log(Application.dataPath);
+}
+
+function Update () {
+	if(Input.GetKeyDown(KeyCode.LeftShift))
+	{
+		Application.CaptureScreenshot("Screen_"+  i+".png",1);
+		i++;
+	}
+}
+
+function ScreenShotMobile()
+{
+	//AndroidCamera.instance.SaveScreenshotToGallery();
+}

7c8e38536c5b7725a84427d6d6a4ffd3744db7ad
Alex Solenyi <saltsav@yandex.ru>
2017-09-12 16:34:01 +0300

    divide

--- Assets/Mega/Scripts/StateOne.cs
+++ Assets/Mega/Scripts/StateOne.cs
@@ -9,7 +9,7 @@ namespace Assets.Mega.Scripts {
         public ViewStates viewStates;
         private TypeCameraOnState typeCameraOnState = TypeCameraOnState.perspective;
 
-        public Transform StateLookTransform;
+        //public Transform StateLookTransform;
 
         public void OnPointerDown (PointerEventData data) {
             //Debug.Log(gameObject.name);
@@ -36,9 +36,12 @@ namespace Assets.Mega.Scripts {
             MegaCameraController.inst.distansAllMega = GlobalParams.distansOnAllMega;
             MegaCameraController.inst.stateLookVector3AllMega = new Vector3(12f, 0, -70f);
             Video.inst.FadeOn();
-            MainLogic.inst.interfaceMega.SetActive(false);
+            if (MainLogic.inst.interfaceMega) {
+                MainLogic.inst.interfaceMega.SetActive(false);
+            }
+            
             imageOne.raycastTarget = true;
-            MegaCameraController.inst.SetNewPosCamera(StateLookTransform.position, GlobalParams.eulerAnglesForCameraInAllMega, 
+            MegaCameraController.inst.SetNewPosCamera(new Vector3(12f, 0, -70f), GlobalParams.eulerAnglesForCameraInAllMega, 
                 GlobalParams.fieldOfViewOnStateOne, GlobalParams.distansOnStateOne, TypeMoveCamera.slow);
         }
 

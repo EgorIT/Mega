c7e0d98e72f0a3dac3a88df3a4fcfbae7e9f831c
EgorIT <31497029+EgorIT@users.noreply.github.com>
2017-09-12 19:34:20 +0300

    *ShopIcons

---
+++ Assets/Mega/Scripts/Interface/ShopPreviews.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopPreviews : MonoBehaviour
+{
+
+	public List<Sprite> sprites;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

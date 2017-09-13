6230ab20b2b6dcd710eb6c8841aa995d9114683c
EgorIT <31497029+EgorIT@users.noreply.github.com>
2017-09-13 19:52:55 +0300

    *for button

---
+++ Assets/Mega/Scripts/Interface/GoToShop.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoToShop : MonoBehaviour
+{
+
+	private AllCaps allCaps;
+	private Image image;
+	
+	public void PushButton()
+	{
+		ShopCap shopCap = allCaps.allCaps.First(x => x.GetComponent<ShopCap>().name == image.name);
+		if (shopCap != null)
+		{
+			shopCap.MoveCamera();
+		}
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		allCaps = GameObject.FindObjectOfType<AllCaps>();
+		image = gameObject.GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

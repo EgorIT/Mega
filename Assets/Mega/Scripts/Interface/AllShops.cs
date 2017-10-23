a30e08c7cb83dbddb6a124c381e0abf3ed334f25
EgorIT <31497029+EgorIT@users.noreply.github.com>
2017-10-23 14:54:23 +0300

    *debug

--- Assets/Mega/Scripts/Interface/AllShops.cs
+++ Assets/Mega/Scripts/Interface/AllShops.cs
@@ -52,7 +52,7 @@ db.Add(new ChangesInfo.Shop("Benetton", "",""));
 db.Add(new ChangesInfo.Shop("Bershka", "",""));
 db.Add(new ChangesInfo.Shop("Betkhoven", "",""));
 db.Add(new ChangesInfo.Shop("BORK", "",""));
-db.Add(new ChangesInfo.Shop("BOBBI BROWN", "",""));
+//db.Add(new ChangesInfo.Shop("BOBBI BROWN", "",""));
 db.Add(new ChangesInfo.Shop("Calvin Klein Jeans", "",""));
 db.Add(new ChangesInfo.Shop("Calzedonia", "",""));
 db.Add(new ChangesInfo.Shop("CAMP DAVID", "",""));
